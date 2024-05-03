bbc647b66cb80309f83e838c5f5fefba5d3251d5
sonih0005 <sonih0005@gmail.com>
2024-05-03 11:32:27 +0530

    some changes on routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -39,10 +39,10 @@ router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
-router.route("/avatar").patch(verifyJWT, upload.single, updateUserAvatar);
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 router
   .route("/coverImage")
-  .patch(verifyJWT, upload.single, updateUserCoverImage);
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 router.route("/watchHistory").get(verifyJWT, getWatchHistory);
 

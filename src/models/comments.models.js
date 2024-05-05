4d840bc40a740e30455a049fe032082dd89b8dd4
sonih0005 <sonih0005@gmail.com>
2024-05-05 19:32:25 +0530

    Add comment, like, playlist, tweet models and write code for comments model

---
+++ src/models/comments.models.js
@@ -0,0 +1,20 @@
+import mongoose, {Schema, mongo} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const commentSchema = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    video: {
+        type: mongoose.Types.ObjectId,
+        ref: "Video"
+    },
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: "User"
+    }
+},{ timestamps: true});
+
+commentSchema.plugin(mongooseAggregatePaginate);
+export const Comment = mongoose.model("Comment", commentSchema)

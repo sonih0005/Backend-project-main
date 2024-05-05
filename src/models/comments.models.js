7b3a4b465f040271c65aab788caf5cd22cfd0ea3
sonih0005 <sonih0005@gmail.com>
2024-05-05 19:39:37 +0530

    write playlist model

--- src/models/comments.models.js
+++ src/models/comments.models.js
@@ -1,4 +1,4 @@
-import mongoose, {Schema, mongo} from "mongoose";
+import mongoose, {Schema} from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const commentSchema = new Schema({
